18fa6216a6e9ecee53fa5cedaca4d1be0a45f6e3
Günther Hutter <guenther.hutter@bytebang.at>
2023-10-04 19:42:32 +0200

    Added a few new commands

--- studio/radio/ic705.js
+++ studio/radio/ic705.js
@@ -95,6 +95,45 @@ Blockly.Lua['ic705_set_af_level'] = function(block) {
 actions.push({"kind": "block", "type": "ic705_set_af_level"});
 
 //-------------------------------------------------------------------
+Blockly.Blocks['ic705_set_frequency'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "Set Frequency to %1 %2",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "FREQUENCY",
+          "check": "Number"
+        },
+        {
+          "type": "field_dropdown",
+          "name": "UNIT",
+          "options": [
+            ["Hz", "1"],
+            ["kHz", "1e3"],
+            ["MHz", "1e6"]
+          ]
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 300,
+      "tooltip": "Sets the frequency of the radio"
+    });
+  }
+};
+
+Blockly.Lua['ic705_set_frequency'] = function(block) {
+  var frequency = Blockly.Lua.valueToCode(block, 'FREQUENCY', Blockly.Lua.ORDER_ATOMIC);
+  var unit = block.getFieldValue('UNIT');
+  use(['frequencyToBcd']);
+  var code = 'sendCommand({0x05}, frequencyToBcd(' + frequency + '*' + unit + ',5))';  
+  return code + ';\n';
+};
+
+actions.push({"kind": "block", "type": "ic705_set_frequency"});
+//-------------------------------------------------------------------
 
 Blockly.Blocks['ic705_set_rf_gain'] = {
   init: function() {
@@ -138,8 +177,8 @@ Blockly.Lua['ic705_set_rf_gain'] = function(block) {
 };
 
 actions.push({"kind": "block", "type": "ic705_set_rf_gain"});
-
 //-------------------------------------------------------------------
+
 Blockly.Blocks['ic705_set_squelch_level'] = {
   init: function() {
     this.jsonInit({
@@ -182,9 +221,171 @@ Blockly.Lua['ic705_set_squelch_level'] = function(block) {
 };
 
 actions.push({"kind": "block", "type": "ic705_set_squelch_level"});
+//-------------------------------------------------------------------
+Blockly.Blocks['ic705_set_mic_gain'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "Set Mic Gain to %1 %%",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "LEVEL",
+          "check": "Number"
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 240,
+      "tooltip": "Sets the mic gain level of the radio (in %)"
+    });
+  }
+};
+
+Blockly.Lua['ic705_set_mic_gain'] = function(block) {
+  var level = Blockly.Lua.valueToCode(block, 'LEVEL', Blockly.Lua.ORDER_ATOMIC);
+  use(['map', 'numberToBcd']);
+  var code = 'sendCommand({0x14, 0x0B, numberToBcd(map(' + level + ', 0, 100, 0, 255))})'; 
+  return code + ';\n';
+};
+actions.push({"kind": "block", "type": "ic705_set_mic_gain"});
+//-------------------------------------------------------------------
+Blockly.Blocks['ic705_set_tuning_step'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "Set Tuning Step to %1",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "STEP",
+          "options": [
+            ["OFF (10Hz or 1Hz)", "0x00"],
+            ["100Hz", "0x01"],
+            ["500Hz", "0x02"],
+            ["1kHz", "0x03"],
+            ["5kHz", "0x04"],
+            ["6.25kHz", "0x05"],
+            ["8.33kHz", "0x06"],
+            ["9kHz", "0x07"],
+            ["10kHz", "0x08"],
+            ["12.5kHz", "0x09"],
+            ["20kHz", "0x0A"],
+            ["25kHz", "0x0B"],
+            ["50kHz", "0x0C"],
+            ["100kHz", "0x0D"]
+          ]
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 280,
+      "tooltip": "Sets the tuning step of the radio"
+    });
+  }
+};
+
+Blockly.Lua['ic705_set_tuning_step'] = function(block) {
+  var step = block.getFieldValue('STEP');
+  var code = 'sendCommand({0x10, ' + step + '})';  
+  return code + ';\n';
+};
+
+actions.push({"kind": "block", "type": "ic705_set_tuning_step"});
+//-------------------------------------------------------------------
+
+Blockly.Blocks['ic705_set_lcd_brightness'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "Set LCD Brightness to %1 %%",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "LEVEL",
+          "check": "Number"
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 260,
+      "tooltip": "Sets the LCD brightness level of the radio (in %)"
+    });
+  }
+};
+
+Blockly.Lua['ic705_set_lcd_brightness'] = function(block) {
+  var level = Blockly.Lua.valueToCode(block, 'LEVEL', Blockly.Lua.ORDER_ATOMIC);
+  use(['map', 'numberToBcd']);
+  var code = 'sendCommand({0x14, 0x19, numberToBcd(map(' + level + ', 0, 100, 0, 255))})'; 
+  return code + ';\n';
+};
+actions.push({"kind": "block", "type": "ic705_set_lcd_brightness"});
+//-------------------------------------------------------------------
+
 /* **************************************************************** */
 /*                          PROPERTIES                              */
 /* **************************************************************** */
+
+Blockly.Blocks['ic705_get_swr_meter_level'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "SWR Meter Level",
+      "output": "Number",
+      "colour": 270,
+      "tooltip": "Reads the current SWR Meter level of the radio"
+    });
+  }
+};
+
+Blockly.Lua['ic705_get_swr_meter_level'] = function(block) {
+  use(['bcdToNumber']);
+  var code = 'bcdToNumber(readResultOfCommand(sendCommand({0x15, 0x12})))';  
+  return [code, Blockly.Lua.ORDER_ATOMIC];
+};
+properties.push({"kind": "block",  "type": "ic705_get_swr_meter_level"})
+//-------------------------------------------------------------------
+
+Blockly.Blocks['ic705_get_lcd_brightness'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "LCD Brightness",
+      "output": "Number",
+      "colour": 260,
+      "tooltip": "Reads the current LCD brightness level of the radio (in %)"
+    });
+  }
+};
+
+Blockly.Lua['ic705_get_lcd_brightness'] = function(block) {
+  use(['map', 'bcdToNumber']);
+  var code = 'math.floor(map(bcdToNumber(readResultOfCommand(sendCommand({0x14, 0x19}))), 0, 255, 0, 100))';  
+  return [code, Blockly.Lua.ORDER_ATOMIC];
+};
+
+properties.push({"kind": "block",  "type": "ic705_get_lcd_brightness"})
+//-------------------------------------------------------------------
+
+Blockly.Blocks['ic705_get_mic_gain'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "Mic Gain",
+      "output": "Number",
+      "colour": 240,
+      "tooltip": "Reads the current mic gain level of the radio (in %)"
+    });
+  }
+};
+
+Blockly.Lua['ic705_get_mic_gain'] = function(block) {
+  use(['map', 'bcdToNumber']);
+  var code = 'math.floor(map(bcdToNumber(readResultOfCommand(sendCommand({0x14, 0x0B}))), 0, 255, 0, 100))'; 
+  return [code, Blockly.Lua.ORDER_ATOMIC];
+};
+
+properties.push({"kind": "block",  "type": "ic705_get_mic_gain"})
+//-------------------------------------------------------------------
+
 Blockly.Blocks['ic705_get_operating_frequency'] = {
   init: function() {
       this.jsonInit({
